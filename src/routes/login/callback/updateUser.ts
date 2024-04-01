53c0008c46608fa9b702744c237a8b59a1b46781
Satpal Singh Rathore <satpal.code@gmail.com>
2024-04-01 16:00:04 +0530

    fix signin issue
    Fixes issue #2
    Earlier user was required to signin twice for Indic chat to recognise.

--- src/routes/login/callback/updateUser.ts
+++ src/routes/login/callback/updateUser.ts
@@ -78,8 +78,6 @@ export async function updateUser(params: {
 			expiresAt: addWeeks(new Date(), 2),
 		});
 
-		// refresh session cookie
-		refreshSessionCookie(cookies, secretSessionId);
 	} else {
 		// user doesn't exist yet, create a new one
 		const { insertedId } = await collections.users.insertOne({
@@ -126,6 +124,9 @@ export async function updateUser(params: {
 			});
 		}
 	}
+
+	// refresh session cookie
+	refreshSessionCookie(cookies, secretSessionId);
 
 	// migrate pre-existing conversations
 	await collections.conversations.updateMany(
